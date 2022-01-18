f0ad99bbad8cbe90e2b983f5cf6938e614e1dd52
goslyan <rezister813@gmail.com>
2022-01-18 19:12:02 +0300

    nice work with sass & gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const {src, dest, watch} = require('gulp');
+const browserSync  = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+
+// static server
+ function bs() {
+    serveSass();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+};
+
+// Compile sass into CSS & auto-inject into browsers
+ function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
