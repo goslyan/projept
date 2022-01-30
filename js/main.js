cb2491512492edee68c8adb96b6aaf10f7905f60
goslyan <rezister813@gmail.com>
2022-01-30 12:40:12 +0300

    12 lesson
    jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function(event) { 
     const modal = document.querySelector('.modal');
     const closeBtn = document.querySelector('.modal__close');
@@ -12,3 +13,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
     closeBtn.addEventListener('click', switchModal);
 
   });
+*/
+
+$(document).ready(function () {
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle=modal]'),
+        closeBtn = $('.modal__close');
+
+    modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+    closeBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+});
